dbd8c466a085a1b830dfad1e9f18bbfb3154bda3
Stephen Johnson <srj15@case.edu>
2011-09-30 01:12:11 -0400

    Beginnings of the page

---
+++ simple_example_out.sh
@@ -0,0 +1,21 @@
+> schemutt simple_example.xml
+simple_example.xml
+<PathwayLayout>
+  <Nodes>
+    <NodeLayout>
+      Y = {'879.5694734042553', '876.2212021276594', '1079.5484000000001'}
+      X = {'221.26220119148942', '1612.846808510638', '472.4837841702128'}
+      ID = {'6054abb3-dc31-43a0-9bd6-b9dda7af77e5'}
+      NeighboringProcessId = {'14fd303b-b106-4e7a-b51a-6d2f9b3d2805'}
+      cofactor = {'i', ' ', 'c'}
+  <Edges>
+    <EdgeLayout>
+      SourceID = {'6054abb3-dc31-43a0-9bd6-b9dda7af77e5'}
+      TargetCofactor = {' '}
+      TargetID = {'c03a2447-fff2-4ef8-896c-65fd36be0ab8'}
+      SourceNeighboringProcessId = {'8c8b1006-b06e-4c59-9b1a-84df2c6d0faf'}
+      TargetNeighboringProcessId = {'14fd303b-b106-4e7a-b51a-6d2f9b3d2805'}
+      SourceCofactor = {'i', ' ', 'c'}
+      <BendPoint>
+        Y = {'1080.210644335484', '787.2429629629629', '683.8948148148148'}
+        X = {'1379.7704548254878', '1378.709530510858', '1574.5530864197533'}
